ca6b5ac6f31a6cbd307a49a2ecfb73a4c7030811
Orkhan <gasanov.orkhan1@gmail.com>
2025-07-31 22:56:53 -0700

    Adding linear regression

--- supervised_learning/01.linear_regression.py
+++ supervised_learning/01.linear_regression.py
@@ -1,11 +1,23 @@
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.array([[1], [2], [3], [4], [5]])
-y = np.array([50, 60, 65, 70, 75])
+# Sample Data: House size vs. Price
+x = np.array([[500], [750], [1000], [1250], [1500]])
+y = np.array([100, 150, 200, 250, 300])  # in $1000s
 
+# Create and train the model
 model = LinearRegression()
 model.fit(x, y)
 
-print("Slope (m):", model.coef_)
-print("Intercept (b):", model.intercept_)
+# Predict the price of a 1100 sq ft house
+predicted_price = model.predict([[1100]])
+print(f"Predicted price: ${predicted_price[0]*1000:.2f}")
+
+# Plotting
+plt.scatter(x, y, color='blue')
+plt.plot(x, model.predict(x), color='red')
+plt.title("Linear Regression - House Price Prediction")
+plt.xlabel("Size (sq ft)")
+plt.ylabel("Price ($1000s)")
+plt.show()
